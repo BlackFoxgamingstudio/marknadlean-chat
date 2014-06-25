010de4126d32af20cca9a85aa94d0299537bd4d9
Mike <m@thecoded.com>
2014-06-25 14:45:24 -0400

    added chat... and running test

---
+++ cloud/models/chat/index.php
@@ -0,0 +1,46 @@
+<?php
+
+	//include model func)
+	require_once($_SERVER['DOCUMENT_ROOT'].'cloud/models/chat/index.php');
+	
+
+	extract($_REQUEST);
+
+	if(!isset($userN)){
+
+		$resp = array("status"=>"fail", "reason"=> "please send userN request header parameter");
+		echo(json_encode($resp));
+		return;
+	}
+	if(!isset($mes)){
+
+		$resp = array("status"=>"fail", "reason"=> "please send message request header parameter");
+		echo(json_encode($resp));
+		exit;
+	}
+
+	if(!isset($action)){
+
+		$resp = array("status"=>"fail", "reason"=> "please send action request header parameter");
+		echo(json_encode($resp));
+		exit;
+	}
+
+	//made it
+	switch($action){
+
+		case "addGet":
+
+			addGetChat($userN, $mes);
+		break;
+
+		default:
+
+			$resp = array("status"=>"fail", "reason"=> "please send userN request header parameter");
+			echo(json_encode($resp));
+		return;
+
+		break;
+	}
+
+?>
